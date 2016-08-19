e4ee27fe287e6bea39b5a817936cf44daf8e3f8e
naomichi agata <naomichi-agata@cookpad.com>
2016-08-19 15:16:20 +0900

    Add test

---
+++ test.sh
@@ -0,0 +1,16 @@
+set -ue
+
+for js in js/*.js
+do
+    if [[ "$js" =~ "while" ]]; then
+        echo $js : skip...
+        continue
+    fi
+    if [[ "$js" =~ "continue" ]]; then
+        echo $js : skip...
+        continue
+    fi
+    echo $js
+    ./bin/jetspider $js
+    ./bin/jsvm ${js}c
+done
