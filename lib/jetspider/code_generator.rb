654b3647cffddd101e5de7907553c4fc39ed16ad
naomichi agata <naomichi-agata@cookpad.com>
2016-08-19 11:00:04 +0900

    Implement condtional expression

--- lib/jetspider/code_generator.rb
+++ lib/jetspider/code_generator.rb
@@ -163,7 +163,15 @@ module JetSpider
     end
 
     def visit_ConditionalNode(n)
-      raise NotImplementedError, 'ConditinalNode'
+      visit n.conditions
+      else_loc = @asm.lazy_location
+      merge_loc = @asm.lazy_location
+      @asm.ifeq(else_loc)
+      visit n.value
+      @asm.goto(merge_loc)
+      @asm.fix_location(else_loc)
+      visit n.else
+      @asm.fix_location(merge_loc)
     end
 
     def visit_WhileNode(n)
